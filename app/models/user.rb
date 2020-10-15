13f2d08156e36492a6a49e54b75c722faabfc664
daichi-tech <daidaihaster@yahoo.co.jp>
2020-10-15 16:42:05 +0900

    usersテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :email, uniqueness: true
+         validates :password, format: { with: /[a-z\d]{6,}/i}
+         validates :nickname, presence: true, length: { maximum: 40 }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+         validates :birthday, presence: true
 end
