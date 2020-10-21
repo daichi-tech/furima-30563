65ed18fed32bd6b79874629fd55dbb6242e8ac69
daichi-tech <daidaihaster@yahoo.co.jp>
2020-10-21 22:51:49 +0900

    購入保存 + エラーハンドリング + formオブジェクト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,36 @@
 class OrdersController < ApplicationController
+
   def index
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new(order_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:purchase).permit(:postal_code, :city, :house_number, :building, :phone_number, :prefecture_id, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_124146) do
     t.string "house_number", null: false
     t.string "building"
     t.string "phone_number", null: false
-    t.integer "prefcture_id", null: false
+    t.integer "prefecture_id", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
